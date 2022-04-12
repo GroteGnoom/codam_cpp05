8e21aceeead431f36c0f40784b96e39818ed8cb1
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-04-12 10:39:59 +0200

    add ex01

--- ex00/GradeTooLowException.hpp
+++
@@ -1,9 +0,0 @@
-
-
-class  GradeTooLowException : public std::exception
-{
-	const char *what() const throw ();
-    {
-    	return "C++ Exception";
-    }
-}
---
+++ ex01/main.cpp
@@ -0,0 +1,47 @@
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+#include <iostream>
+
+int main() {
+	std::cout << "-------------Bureaucrats!----------" << std::endl;
+	Bureaucrat a("Ms. Bla", 1);
+	Bureaucrat b("Ms. Bloe", 150);
+	try {
+		Bureaucrat c("Ms. Too low", 250);
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		Bureaucrat c("Ms. Too high", 0);
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+
+	std::cout << "-------------Forms!----------" << std::endl;
+	Form fa("form A", 1, 3);
+	Form fb("form B", 150, 3);
+	try {
+		Form fc("form too low", 250, 3);
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		Form fc("form too high", 0, 3);
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << "form a is " << fa << std::endl;
+	std::cout << "form b is " << fb << std::endl;
+
+	std::cout << "-------------Signing forms!----------" << std::endl;
+	a.signForm(fa);
+	a.signForm(fb);
+	b.signForm(fa);
+	b.signForm(fb);
+}
